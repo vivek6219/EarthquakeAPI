d375eef1d1bc354edbb287d65e8d108d69d52425
vivek6219 <33075276+vivek6219@users.noreply.github.com>
2021-02-21 18:16:34 -0800

    Starter files

---
+++ earthquakes/src/main/java/com/earthquakeviewer/earthquakes/EarthquakesApplication.java
@@ -0,0 +1,13 @@
+package com.earthquakeviewer.earthquakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthquakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthquakesApplication.class, args);
+	}
+
+}
